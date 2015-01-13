47dfb359d5ccfed1f96b81faac4b35f5df7ce3e6
Eric Gelinas <evg@stamen.com>
2015-01-12 17:15:38 -0800

    change bower json settings

--- require_config.js
+++ require_config.js
@@ -8,7 +8,8 @@ require.config({
     normalize: "bower_components/require-css/normalize",
     requirejs: "bower_components/requirejs/require",
     leaflet: "bower_components/leaflet/dist/leaflet",
-    "leaflet-src": "bower_components/leaflet/dist/leaflet-src"
+    "leaflet-src": "bower_components/leaflet/dist/leaflet-src",
+    samesies: "bower_components/samesies/index"
   },
   packages: [
 
