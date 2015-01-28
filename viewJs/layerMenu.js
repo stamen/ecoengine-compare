a3747711914471238519e1a05778a2923183a4e8
Eric Gelinas <evg@stamen.com>
2015-01-27 17:52:04 -0800

    fix ordering

--- viewJs/layerMenu.js
+++ viewJs/layerMenu.js
@@ -2,13 +2,16 @@
 
 function LayerMenuController() {
 
+  var b = 0;
+
   var dragConfig = {
 
     dynamicDrop: true,
 
     onstart: function(event) {
-      event.target.setAttribute('data-x', event.target.offsetParent.offsetLeft);
-      event.target.setAttribute('data-y', event.target.offsetParent.offsetTop);
+
+      event.target.setAttribute('data-x', event.target.offsetLeft-event.target.parentNode.offsetLeft);
+      event.target.setAttribute('data-y', event.target.offsetTop-event.target.parentNode.offsetTop);
 
       event.target.parentNode.classList.add("dragging");
     },
@@ -19,7 +22,10 @@ function LayerMenuController() {
       var target = event.target,
       // keep the dragged position in the data-x/data-y attributes
       x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy,
+
+      isListItem = event.dropzone ? event.dropzone.selector.indexOf("draggable") : "null",
+      dropZone   = event.dropzone ? document.querySelector(event.dropzone.selector) : null;
 
       // translate the element
       target.style.webkitTransform =
@@ -29,13 +35,28 @@ function LayerMenuController() {
       // update the posiion attributes
       target.setAttribute('data-x', x);
       target.setAttribute('data-y', y);
+
+      if (isListItem > -1) {
+        if (dropZone && dropZone.parentNode) {
+          var dragPosState = [[event.pageX, event.pageY], [(dropZone.offsetLeft+dropZone.parentNode.offsetLeft+dropZone.offsetWidth)/2, (dropZone.offsetTop+dropZone.parentNode.offsetTop+dropZone.offsetHeight)/2]];
+
+          if (dragPosState[0][1] < dragPosState[1][1]) {
+            dropZone.style.marginTop = "100px";
+            dropZone.style.marginBottom = "0";
+          } else {
+            dropZone.style.marginTop = "0";
+            dropZone.style.marginBottom = "100px";
+          }
+        }
+      }
     },
     // call this function on every dragend event
     onend: function (event) {
-
       event.target.style["-webkit-transform"] = "translate(0,0)";
       event.target.style["transform"] = "translate(0,0)";
       event.target.parentNode.classList.remove("dragging");
+
+      event.target.parentNode.style.zIndex = "inherit";
 
     }
   };
@@ -44,7 +65,7 @@ function LayerMenuController() {
     // only accept elements matching this CSS selector
     accept: '.drag-drop',
     // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
+    overlap: .1,
 
     // listen for drop related events:
 
@@ -64,17 +85,30 @@ function LayerMenuController() {
       // remove the drop feedback style
       event.target.classList.remove('drop-target');
       event.relatedTarget.classList.remove('can-drop');
+
+      if (event.target.classList.contains("draggable")) {
+        event.target.style.marginTop = "0";
+        event.target.style.marginBottom = "0";
+      }
     },
     ondrop: function dropEvent(event) {
       //console.log("event",event);
-      event.target.appendChild(event.relatedTarget);
+      if (event.target.classList.contains("draggable")) {
+        event.target.parentNode.appendChild(event.relatedTarget);
+        event.target.style.marginTop = "0";
+        event.target.style.marginBottom = "0";
+      } else {
+        event.target.appendChild(event.relatedTarget);
+      }
+
     },
     ondropdeactivate: function (event) {
       // remove active dropzone feedback
       event.target.classList.remove('drop-active');
       event.target.classList.remove('drop-target');
     },
-    ondropmove: function() {
+    ondropmove: function(event) {
+
     }
   };
 
