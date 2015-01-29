74eba10d1c64d095392ec16a0e4f1f6d436ebe77
Eric Gelinas <evg@stamen.com>
2015-01-28 16:56:14 -0800

    allow multiple layers of clustering

--- viewJs/index.js
+++ viewJs/index.js
@@ -10,7 +10,7 @@ function IndexController() {
     map = L.map("map").setView([37.5333, -77.4667], 7);
 
     // add an OpenStreetMap tile layer
-    L.tileLayer("http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png", {
+    L.tileLayer("http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
     }).addTo(map);
 
@@ -27,7 +27,7 @@ function IndexController() {
       //console.log("Done.", pages);
 
       var hulllayer1 = STMN.hullLayer(pages, {
-        color : "blue"
+        color : "red"
       });
       hulllayer1.addTo(map);
 
@@ -42,26 +42,26 @@ function IndexController() {
     });
   }, 1000);
 
-  /*
   setTimeout(function() {
-  STMN.berkeley.requestRecursive("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=kingdom_exact%3A%22animalia%22&&selected_facets=genus_exact%3A%22tamias%22&&selected_facets=resource_exact%3A%22Observations%22&q=&min_date=1970-01-01&max_date=2010-12-30&page_size=2000",
-  function(pages) { //Done
-  //console.log("Done.", pages);
+    STMN.berkeley.requestRecursive("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1930-12-30&max_date=1950-12-30&page_size=2000",
+    function(pages) { //Done
+      //console.log("Done.", pages);
 
-  var hulllayer2 = exports.hullLayer(pages, {
-  color : "red"
-});
-hulllayer2.addTo(map);
+      var hulllayer2 = STMN.hullLayer(pages, {
+        color : "blue"
+      });
+      hulllayer2.addTo(map);
+
+      map.fitBounds(hulllayer2.getBounds());
 
 
 
 
-},
-function(pages) { //Progress
-console.log("Page recieved.", pages);
-});
-}, 100);
-*/
+    },
+    function(pages) { //Progress
+      console.log("Page recieved.", pages);
+    });
+  }, 1000);
 
 }
 
