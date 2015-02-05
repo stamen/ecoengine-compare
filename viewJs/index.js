74cc3df2c731d558bcc922238c4670e487f5f30b
Eric Gelinas <evg@stamen.com>
2015-02-04 16:26:51 -0800

    add test queries to the console output

--- viewJs/index.js
+++ viewJs/index.js
@@ -93,70 +93,13 @@ function IndexController() {
   //
   initMap();
 
-  /*
-  setTimeout(function() {
-    STMN.berkeley.requestRecursive("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1900-01-01&max_date=1930-12-30&page_size=2000",
-    function(pages) { //Done
-      //console.log("Done.", pages);
-
-      var hulllayer1 = STMN.hullLayer(pages, {
-        color : "red"
-      });
-      hulllayer1.addTo(map);
-
-      map.fitBounds(hulllayer1.getBounds());
-
-
-
-
-    },
-    function(pages) { //Progress
-      console.log("Page recieved.", pages);
-    });
-  }, 10);
-
-  setTimeout(function() {
-    STMN.berkeley.requestRecursive("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1930-12-30&max_date=1950-12-30&page_size=2000",
-    function(pages) { //Done
-      //console.log("Done.", pages);
-
-      var hulllayer2 = STMN.hullLayer(pages, {
-        color : "blue"
-      });
-      hulllayer2.addTo(map);
-
-      map.fitBounds(hulllayer2.getBounds());
-
-
-
-
-    },
-    function(pages) { //Progress
-      console.log("Page recieved.", pages);
-    });
-  }, 100);
-
-  setTimeout(function() {
-    STMN.berkeley.requestRecursive("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1950-12-30&max_date=1970-12-30&page_size=2000",
-    function(pages) { //Done
-      //console.log("Done.", pages);
-
-      var hulllayer3 = STMN.hullLayer(pages, {
-        color : "green"
-      });
-      hulllayer3.addTo(map);
-
-      map.fitBounds(hulllayer3.getBounds());
-
-
-
-
-    },
-    function(pages) { //Progress
-      console.log("Page recieved.", pages);
-    });
-  }, 1000);
-  */
+  //
+  // Console log some helpful test queries
+  //
+  console.log("Here are some helpful test queries:");
+  console.log("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1900-01-01&max_date=1930-12-30&page_size=2000");
+  console.log("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1930-12-30&max_date=1950-12-30&page_size=2000");
+  console.log("https://dev-ecoengine.berkeley.edu/api/observations/?format=geojson&selected_facets=genus_exact%3A%22tamias%22&q=&min_date=1950-12-30&max_date=1970-12-30&page_size=2000");
 
   return that;
 
