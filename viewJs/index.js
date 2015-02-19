9621b0dbc03f73b4a2b3eb239f797326658552f5
Eric Gelinas <evg@stamen.com>
2015-02-19 14:51:14 -0800

    more changes

--- viewJs/index.js
+++ viewJs/index.js
@@ -188,12 +188,16 @@ function IndexController() {
 
     var layerNode = layerMenu.getLayerNode(layer);
 
+    layerNode.classList.add("progress");
+
     that.utils.append(layerNode, "<div id=\"floatingCirclesG\" class=\"loading\"><div class=\"f_circleG\" id=\"frotateG_01\"></div><div class=\"f_circleG\" id=\"frotateG_02\"></div><div class=\"f_circleG\" id=\"frotateG_03\"></div><div class=\"f_circleG\" id=\"frotateG_04\"></div><div class=\"f_circleG\" id=\"frotateG_05\"></div><div class=\"f_circleG\" id=\"frotateG_06\"></div><div class=\"f_circleG\" id=\"frotateG_07\"></div><div class=\"f_circleG\" id=\"frotateG_08\"></div></div>");
   }
 
   function hideMenuItemLoadState(layer) {
     var layerNode   = layerMenu.getLayerNode(layer),
         loadingNode = layerNode.querySelector(".loading");
+
+    layerNode.classList.remove("progress");
 
     if (loadingNode) {
       loadingNode.parentNode.removeChild(loadingNode);
